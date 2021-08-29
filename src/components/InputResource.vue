7def701cc6ce85af05495510147ee8142af742a6
Hossein Moradi <ho3inmoradi1998@gmail.com>
2021-08-29 13:55:32 -0700

    add input resource component

---
+++ src/components/InputResource.vue
@@ -0,0 +1,16 @@
+<template>
+    <form>
+        <div>
+            <label>عنوان منبع</label>
+            <input type="text">
+        </div>
+        <div>
+            <label>توضیحاتی در مورد منبع</label>
+            <input type="text">
+        </div>
+        <div>
+            <label>لینک منبع</label>
+            <input type="text">
+        </div>
+    </form>
+</template>
