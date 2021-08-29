c793bb869b79dd2aa2e5a6c65744b2fa8ef1c87b
Hossein Moradi <ho3inmoradi1998@gmail.com>
2021-08-29 13:51:50 -0700

    create stored resource

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,27 @@
     <button @click="setSelectedCmp('stored-resource')">لیست منابع آموزشی</button>
     <button @click="setSelectedCmp('input-resource')">وارد کردن منبع آموزشی</button>
 
-    <ul>
-        <li v-for="res in resourcedData" :key="res">
-            {{ res.title}}
-        </li>
-    </ul>
+    <stored-resource
+            v-for="res in resourcedData"
+            :key="res.id"
+            :title="res.title"
+            :description="res.description"
+            :link="res.link"
+    ></stored-resource>
+    <input-resource></input-resource>
+
 </template>
 
 <script>
     import TheHeader from './components/UI/Header';
+    import StoredResource from './components/StoredResource';
+    import InputResource from './components/InputResource';
+
     export default{
         components:{
             'the-header':TheHeader,
+            'stored-resource':StoredResource,
+            'input-resource':InputResource,
         },
         data(){
             return{
@@ -25,13 +34,13 @@
                         id:'learning-vue',
                         title:'سایت اصلی vue.js',
                         description:'بهترین منبع برای مطالعه داکیومنت ویو',
-                        link:'vuejs.org'
+                        link:'https://www.vuejs.org'
                     },
                     {
                         id:'vue-school',
                         title:'سایت مدرسه ویو',
                         description:'بهترین سایت برای یادگیری ویو به صورت تصویری',
-                        link:'vueschool.io'
+                        link:'https://www.vueschool.io'
                     },
                 ]
             }
@@ -39,10 +48,8 @@
         methods:{
             setSelectedCmp(cmp){
                 if (cmp === 'stored-resource'){
-                    console.log(cmp);
                     this.selectedCmp = 'stored-resource';
                 }else{
-                    console.log(cmp);
                     this.selectedCmp = 'input-resource';
                 }
             }
---
+++ src/components/StoredResource.vue
@@ -0,0 +1,15 @@
+<template>
+    <ul>
+        <li>
+            <h4>{{ title }}</h4>
+            <p>{{ description }}</p>
+            <a :href="link">{{ link }}</a>
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        props:['title','description','link']
+    }
+</script>
